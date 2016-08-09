4a1f83389bd5012e88814c7a68088a2a2bbe23a7
Vyacheslav Kryuchenko <slavyan.85@mail.ru>
2016-08-09 21:53:14 +0300

    internal: fix build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 go get -u bitbucket.org/slavyan85/svnwrapper
 export GOPATH=$GOPATH:`pwd`
 
-go build -o SvnDataLoader src\svndataloader.go
+go build -o SvnDataLoader src/svndataloader.go
 
 export GOOS=windows
 export GOARCH=amd64
-go build -o SvnDataLoader.exe src\svndataloader.go
+go build -o SvnDataLoader.exe src/svndataloader.go
